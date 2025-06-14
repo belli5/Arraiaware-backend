fac5fa66a8499ea18cdc1b3f417a139503842216
NathanDalbert <dalbert.nathan@gmail.com>
2025-06-14 20:55:12 -0300

    feat(app): integra o RolesModule na aplicação principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { RolesModule } from './roles/roles.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [PrismaModule, UsersModule, RolesModule],
   controllers: [AppController],
   providers: [AppService],
 })
