2ef8e9225a99c0385a5903b276d9ca0ce4dcc0df
NathanDalbert <dalbert.nathan@gmail.com>
2025-06-14 21:24:59 -0300

    eat(app): integra ofeat(criteria): cria o módulo de critérios  CriteriaModule ao AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CriteriaModule } from './criteria/criteria.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { RolesModule } from './roles/roles.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, RolesModule],
+  imports: [PrismaModule, UsersModule, RolesModule, CriteriaModule],
   controllers: [AppController],
   providers: [AppService],
 })
