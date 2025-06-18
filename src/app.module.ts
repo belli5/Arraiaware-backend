a4e6b4652067cf06cdab0a332e545d3d11ca9d6b
NathanDalbert <dalbert.nathan@gmail.com>
2025-06-18 14:41:41 -0300

    feat(app): Adiciona o módulo de RH ao aplicativo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { EvaluationsModule } from './evaluations/evaluations.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { RolesModule } from './roles/roles.module';
 import { UsersModule } from './users/users.module';
+import { RhModule } from './rh/rh.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { UsersModule } from './users/users.module';
     CriteriaModule,
     CyclesModule,
     EvaluationsModule,
+    RhModule,
   ],
   controllers: [AppController],
   providers: [AppService],
