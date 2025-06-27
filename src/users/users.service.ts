d511fb79d52684989b96fafbfa4f2b65d0f53d13
NathanDalbert <dalbert.nathan@gmail.com>
2025-06-27 12:58:03 -0300

    feat(auth): adicionar funcionalidades de alteração e redefinição de senha

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,18 +2,27 @@ import {
   ConflictException,
   Injectable,
   NotFoundException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import { randomBytes } from 'crypto';
+import { EmailService } from '../email/email.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { ChangePasswordDto } from './dto/change-password.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private emailService: EmailService,
+  ) {}
 
   async create(createUserDto: CreateUserDto) {
-    const { email, password, name, userType,unidade, roleId, leaderId } = createUserDto;
+    const { email, name, userType, unidade, roleId, leaderId } = createUserDto;
+    let password = createUserDto.password;
+    let passwordToSend: string | null = null;
 
     const existingUser = await this.prisma.user.findUnique({
       where: { email },
@@ -23,14 +32,20 @@ export class UsersService {
       throw new ConflictException(`O e-mail '${email}' já está em uso.`);
     }
 
+    if (!password) {
+      const generatedPassword = randomBytes(8).toString('hex');
+      password = generatedPassword;
+      passwordToSend = generatedPassword;
+    }
+
     const saltOrRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltOrRounds);
+    const passwordHash = await bcrypt.hash(password, saltOrRounds);
 
     const user = await this.prisma.user.create({
       data: {
         name,
         email,
-        passwordHash: hashedPassword,
+        passwordHash,
         userType,
         unidade,
         roleId,
@@ -38,8 +53,32 @@ export class UsersService {
       },
     });
 
+    if (passwordToSend) {
+      await this.emailService.sendWelcomeEmail(user.email, passwordToSend);
+    }
+
     const { passwordHash: _, ...result } = user;
     return result;
+  }
+  
+  async createAdmin(adminData: { name: string; email: string; userType: any; unidade: string; }) {
+    const { email, name, userType, unidade } = adminData;
+    
+    const saltOrRounds = 10;
+    const generatedPassword = randomBytes(8).toString('hex');
+    const passwordHash = await bcrypt.hash(generatedPassword, saltOrRounds);
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        passwordHash,
+        userType,
+        unidade,
+      },
+    });
+    
+    return generatedPassword; 
   }
 
   async findAll() {
@@ -50,7 +89,7 @@ export class UsersService {
       },
     });
     return users.map((user) => {
-      const { passwordHash: _, ...result } = user;
+      const { passwordHash, ...result } = user;
       return result;
     });
   }
@@ -71,21 +110,10 @@ export class UsersService {
 
   async update(id: string, updateUserDto: UpdateUserDto) {
     await this.findOne(id);
-
-    const dataToUpdate: any = { ...updateUserDto };
-    delete dataToUpdate.password;
-
-    if (updateUserDto.password) {
-      const saltOrRounds = 10;
-      dataToUpdate.passwordHash = await bcrypt.hash(
-        updateUserDto.password,
-        saltOrRounds,
-      );
-    }
-
+    
     const updatedUser = await this.prisma.user.update({
       where: { id },
-      data: dataToUpdate,
+      data: updateUserDto,
     });
 
     const { passwordHash: _, ...result } = updatedUser;
@@ -97,7 +125,7 @@ export class UsersService {
     await this.prisma.user.delete({ where: { id } });
     return { message: `Usuário com ID ${id} removido com sucesso.` };
   }
-  
+
   async findByEmail(email: string) {
     return this.prisma.user.findUnique({
       where: { email },
@@ -105,5 +133,55 @@ export class UsersService {
         role: true,
       },
     });
+  }
+
+  async changePassword(userId: string, changePasswordDto: ChangePasswordDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      throw new NotFoundException(`Usuário com ID ${userId} não encontrado.`);
+    }
+
+    const isPasswordMatching = await bcrypt.compare(
+      changePasswordDto.currentPassword,
+      user.passwordHash,
+    );
+
+    if (!isPasswordMatching) {
+      throw new UnauthorizedException('A senha atual está incorreta.');
+    }
+
+    const saltOrRounds = 10;
+    const newPasswordHash = await bcrypt.hash(changePasswordDto.newPassword, saltOrRounds);
+
+    await this.prisma.user.update({
+      where: { id: userId },
+      data: { passwordHash: newPasswordHash },
+    });
+
+    return { message: 'Senha alterada com sucesso.' };
+  }
+
+  async resetPassword(userId: string) {
+    const user = await this.prisma.user.findUnique({ where: { id: userId } });
+    if (!user) {
+      throw new NotFoundException(`Usuário com ID ${userId} não encontrado`);
+    }
+
+    const newPassword = randomBytes(8).toString('hex');
+    const saltOrRounds = 10;
+    
+    const newPasswordHash = await bcrypt.hash(newPassword, saltOrRounds);
+
+    await this.prisma.user.update({
+      where: { id: userId },
+      data: { passwordHash: newPasswordHash },
+    });
+
+    await this.emailService.sendWelcomeEmail(user.email, newPassword);
+
+    return { message: `Uma nova senha temporária foi enviada para o e-mail ${user.email}.` };
   }
 }
