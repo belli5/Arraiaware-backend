33730450fbaf473bc04185f4328fa4100f6038bc
NathanDalbert <dalbert.nathan@gmail.com>
2025-06-27 12:58:17 -0300

    feat(users): adicionar EmailModule às dependências do UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { EmailModule } from 'src/email/email.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule,EmailModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
+
 })
 export class UsersModule {}
