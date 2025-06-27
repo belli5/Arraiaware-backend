4c3e764c88871e4594cc7af7e73891621a41c530
NathanDalbert <dalbert.nathan@gmail.com>
2025-06-27 12:55:44 -0300

    fix(auth): atualizar exemplos de email e senha no LoginDto

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -2,11 +2,11 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDto {
-  @ApiProperty({ example: 'exemplo@corp.com' })
+  @ApiProperty({ example: 'admin@rocketcorp.com' })
   @IsEmail()
   email: string;
 
-  @ApiProperty({ example: 'senha123' })
+  @ApiProperty({ example: 'Admin@1234' })
   @IsString()
   @IsNotEmpty()
   password: string;
