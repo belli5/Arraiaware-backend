0f24db8398bfa4e80086a781c1e750b606902b0b
NathanDalbert <dalbert.nathan@gmail.com>
2025-06-28 10:51:38 -0300

    feat(dto): criar DTO TrilhaResponseDto com critérios para a trilha

---
+++ src/roles/dto/trilha-response.dto.ts
@@ -0,0 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+class CriterionDto {
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty()
+  pillar: string;
+
+  @ApiProperty()
+  criterionName: string;
+
+  @ApiProperty()
+  description: string;
+}
+export class TrilhaResponseDto {
+  @ApiProperty({ description: 'Nome da trilha' })
+  nome_da_trilha: string;
+
+  @ApiProperty({ type: [CriterionDto], description: 'Lista de critérios da trilha' })
+  criterios: CriterionDto[];
+}
