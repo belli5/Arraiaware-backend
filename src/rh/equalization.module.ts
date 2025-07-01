f754e4f975a1a6ca1212c96a359288098a1ed4be
Mickael-7 <mickael.albuquerque@upe.br>
2025-07-01 13:07:18 -0300

    feat(equalization): adicionar NotificationsModule às importações do EqualizationModule

--- src/rh/equalization.module.ts
+++ src/rh/equalization.module.ts
@@ -3,9 +3,10 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { EqualizationController } from './equalization.controller';
 import { EqualizationService } from './equalization.service';
 import { GenAIModule } from 'src/gen-ai/gen-ai.module';
+import { NotificationsModule } from 'src/notifications/notifications.module';
 
 @Module({
-  imports: [PrismaModule, GenAIModule],
+  imports: [PrismaModule, GenAIModule,NotificationsModule],
   controllers: [EqualizationController],
   providers: [EqualizationService],
 })
