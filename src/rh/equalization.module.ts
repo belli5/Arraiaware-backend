a28fabe7e8cc1ea775ad31bafbc3706f85331f65
Mickael-7 <mickael.albuquerque@upe.br>
2025-07-01 12:12:17 -0300

    feat(equalization): adicionar GenAIModule às importações do módulo de equalização

--- src/rh/equalization.module.ts
+++ src/rh/equalization.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { EqualizationController } from './equalization.controller';
 import { EqualizationService } from './equalization.service';
+import { GenAIModule } from 'src/gen-ai/gen-ai.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, GenAIModule],
   controllers: [EqualizationController],
   providers: [EqualizationService],
 })
