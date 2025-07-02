bf5a900f957c1d18c86764bf946d03a12d049db3
Mickael-7 <mickael.albuquerque@upe.br>
2025-07-02 15:40:27 -0300

    feat(committee):adicionar GenAIModule e EqualizationModule às importações do CommitteeModule

--- src/comitte/committee.module.ts
+++ src/comitte/committee.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { GenAIModule } from '../gen-ai/gen-ai.module';
 import { PrismaModule } from '../prisma/prisma.module';
+import { EqualizationModule } from '../rh/equalization.module';
 import { CommitteeController } from './committee.controller';
 import { CommitteeService } from './committee.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    GenAIModule,
+    EqualizationModule,
+  ],
   controllers: [CommitteeController],
   providers: [CommitteeService],
 })
