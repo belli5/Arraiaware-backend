66951caa913b0640fe5e1aea63db5203fbb56cb1
NathanDalbert <dalbert.nathan@gmail.com>
2025-07-02 19:05:19 -0300

    feat(erp): criar ErpUserDto com campos de usuário para integração no sistema

---
+++ src/erp/dto/erp-user.dto.ts
@@ -0,0 +1,10 @@
+export class ErpUserDto {
+  id_colaborador: string;
+  nome_completo: string;
+  email_corporativo: string;
+  id_lider: string | null;
+  nome_lider: string;
+  id_projeto: string;
+  nome_projeto: string;
+  unidade_negocio: string;
+}
