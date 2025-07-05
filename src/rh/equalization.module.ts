57ae059a08ae887a17f3321df458d303899421dc
Mickael-7 <mickael.albuquerque@upe.br>
2025-07-04 21:29:46 -0300

    feat(rh): adicionar PdfModule às dependências do EqualizationModule

--- src/rh/equalization.module.ts
+++ src/rh/equalization.module.ts
@@ -4,9 +4,10 @@ import { EqualizationController } from './equalization.controller';
 import { EqualizationService } from './equalization.service';
 import { GenAIModule } from 'src/gen-ai/gen-ai.module';
 import { NotificationsModule } from 'src/notifications/notifications.module';
+import { PdfModule } from 'src/pdf/pdf.module';
 
 @Module({
-  imports: [PrismaModule, GenAIModule,NotificationsModule],
+  imports: [PrismaModule, GenAIModule,NotificationsModule,PdfModule],
   controllers: [EqualizationController],
   providers: [EqualizationService],
   exports: [EqualizationService],
