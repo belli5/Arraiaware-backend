2aa9f3bb623402c1f3a88052cfab67db2c0e1b15
NathanDalbert <dalbert.nathan@gmail.com>
2025-07-07 10:56:33 -0300

    feat(evaluations): adicionar EncryptionModule ao módulo de avaliações

--- src/evaluations/evaluations.module.ts
+++ src/evaluations/evaluations.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { EncryptionModule } from '../common/encryption/encryption.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { EvaluationsController } from './evaluations.controller';
 import { EvaluationsService } from './evaluations.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    EncryptionModule, 
+  ],
   controllers: [EvaluationsController],
   providers: [EvaluationsService],
+  exports: [EvaluationsService],
 })
 export class EvaluationsModule {}
