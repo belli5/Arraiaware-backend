be6e3e36769f3daad680a09de4bdfe356765ba76
NathanDalbert <dalbert.nathan@gmail.com>
2025-07-07 09:59:38 -0300

    feat(encryption): adicionar controlador ao módulo de criptografia

--- src/common/encryption/encryption.module.ts
+++ src/common/encryption/encryption.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { EncryptionController } from './encryption.controller';
 import { EncryptionService } from './encryption.service';
 
 @Module({
   imports: [ConfigModule], 
+  controllers: [EncryptionController],
   providers: [EncryptionService],
   exports: [EncryptionService], 
 })
