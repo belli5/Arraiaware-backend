de91ad47566f36d15f72c1d257325827205b2a0b
NathanDalbert <dalbert.nathan@gmail.com>
2025-07-08 15:00:03 -0300

    feat(audit): criar tabela AuditLog com campos necessários e chave estrangeira

---
+++ prisma/migrations/20250708174429_fix_audit_log_relation/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "AuditLog" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT,
+    "action" TEXT NOT NULL,
+    "entity" TEXT,
+    "entityId" TEXT,
+    "details" JSONB,
+    "ipAddress" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "AuditLog_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
