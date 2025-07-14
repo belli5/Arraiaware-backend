44f02e89be107a0908ec8a05f452c5e5a8acd42d
NathanDalbert <dalbert.nathan@gmail.com>
2025-07-14 18:33:45 -0300

    feat(team): adicionar CyclesModule às importações do TeamModule

--- src/team/team.module.ts
+++ src/team/team.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CyclesModule } from '../cycles/cycles.module'; 
 import { PrismaModule } from '../prisma/prisma.module';
 import { TeamController } from './team.controller';
 import { TeamService } from './team.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CyclesModule], 
   controllers: [TeamController],
   providers: [TeamService],
 })
